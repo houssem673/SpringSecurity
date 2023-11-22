c431b1c17688d6d582601aff2f3a214e90f5c29d
Houssem Rajbeni <horaj100@hhu.de>
2023-11-22 21:55:17 +0100

    FEATURE Spring Security FEATURE Configure security

---
+++ src/main/java/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+
+@Bean
+public SecurityFilterChain config(HttpSecurity chainBuilder) throws Exception {
+    chainBuilder.authorizeHttpRequests(
+            configurer -> configurer.requestMatchers("/","/public","/css/*").permitAll()
+                    .anyRequest().authenticated()
+    );
+
+    return chainBuilder.build();
+}
+
+
+}
