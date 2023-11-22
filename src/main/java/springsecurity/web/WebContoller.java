7a2f30443f3c5c08b62d82edd06bfb0416549dad
Houssem Rajbeni <horaj100@hhu.de>
2023-11-22 22:39:12 +0100

    Access User info display Username with Hello message

--- src/main/java/springsecurity/web/WebContoller.java
+++ src/main/java/springsecurity/web/WebContoller.java
@@ -1,6 +1,9 @@
 package springsecurity.web;
 
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -17,7 +20,11 @@ public class WebContoller {
         return "public";
     }
     @GetMapping("/private")
-    public String indexPrivate(){
+    public String indexPrivate(OAuth2AuthenticationToken oauth, Model model ){
+        System.out.println(oauth); //print login details
+        OAuth2User user = oauth.getPrincipal();
+        String loginName = user.getAttribute("login");
+        model.addAttribute("name",loginName);
         return "private";
     }
     @GetMapping("/secret")
